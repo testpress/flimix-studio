9eb5936fbaab83e6608cdb9ed9cf5978545c2a9a
HarishV14 <harishv@testpress.in>
2025-09-30 11:54:51 +0530

    refractor

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import HeaderFooterBuilder from './editor/HeaderFooterEditor.tsx'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HeaderFooterBuilder />
+    <App />
   </StrictMode>,
 )
