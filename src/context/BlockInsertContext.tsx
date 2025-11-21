d8d5f008610021eda0b1738d9ab2555d336b3d84
karthikeyan <karthikeyan@testpress.in>
2025-11-21 13:00:31 +0530

    fix: Remove undefined variable that cause error while build

--- src/context/BlockInsertContext.tsx
+++ src/context/BlockInsertContext.tsx
@@ -294,7 +294,7 @@ export const BlockInsertProvider: React.FC<BlockInsertProviderProps> = ({ childr
    */
   const insertBlockInsideSection = (blockType: BlockType['type'], sectionId: string) => {
     // Find the selected section and its parent
-    const { block: targetContainer, parent: targetParent } = findBlockAndParent(
+    const { block: targetContainer } = findBlockAndParent(
       sectionId,
       pageSchema.blocks,
     );
